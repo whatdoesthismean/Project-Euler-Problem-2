0722cf0862c56bd5bb4a74e021b2cc36d5e12b68
Jeremy Harding <75227382+whatdoesthismean@users.noreply.github.com>
2021-03-21 19:39:33 -0700

    Add project files.

---
+++ Sequence02/Form1.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.lstRecord = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.BackColor = System.Drawing.Color.Green
+        Me.btnCalculate.Font = New System.Drawing.Font("Segoe UI", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnCalculate.Location = New System.Drawing.Point(12, 120)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(345, 378)
+        Me.btnCalculate.TabIndex = 0
+        Me.btnCalculate.Text = "CALCULATE"
+        Me.btnCalculate.UseVisualStyleBackColor = False
+        '
+        'lblTotal
+        '
+        Me.lblTotal.BackColor = System.Drawing.Color.White
+        Me.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTotal.Font = New System.Drawing.Font("Segoe UI", 57.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblTotal.Location = New System.Drawing.Point(12, 14)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(345, 103)
+        Me.lblTotal.TabIndex = 1
+        Me.lblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lstRecord
+        '
+        Me.lstRecord.FormattingEnabled = True
+        Me.lstRecord.ItemHeight = 15
+        Me.lstRecord.Location = New System.Drawing.Point(363, 14)
+        Me.lstRecord.Name = "lstRecord"
+        Me.lstRecord.Size = New System.Drawing.Size(120, 484)
+        Me.lstRecord.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Orange
+        Me.ClientSize = New System.Drawing.Size(495, 509)
+        Me.Controls.Add(Me.lstRecord)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.Text = "Project Euler Problem 2"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents lstRecord As ListBox
+End Class
---
+++ Sequence02/Form1.vb
@@ -0,0 +1,50 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim intNum1 As Integer = 1
+        Dim intNum2 As Integer = 2
+        Dim intNum3 As Integer = 3
+        Dim intTotal As Integer = 2
+
+        Dim intOdd As Integer
+
+        lstRecord.Items.Clear()
+        lstRecord.Items.Add(intNum1)
+        lstRecord.Items.Add(intNum2)
+        lstRecord.Items.Add(intNum3)
+
+        Do While intNum1 <= 4000000 And intNum2 <= 4000000 And intNum3 <= 4000000
+
+            intNum1 = intNum2 + intNum3
+            lstRecord.Items.Add(intNum1)
+            If intNum1 Mod 2 > 0 Then
+                intOdd += intNum1
+            Else
+                intTotal += intNum1
+            End If
+
+            intNum2 = intNum3 + intNum1
+            lstRecord.Items.Add(intNum2)
+            If intNum2 Mod 2 > 0 Then
+                intOdd += intNum2
+            Else
+                intTotal += intNum2
+            End If
+
+            intNum3 = intNum1 + intNum2
+
+            If intNum3 < 4000000 Then
+                lstRecord.Items.Add(intNum3)
+            End If
+
+            If intNum3 Mod 2 > 0 Then
+                intOdd += intNum3
+            Else
+                intTotal += intNum3
+            End If
+
+            lblTotal.Text = CStr(intTotal)
+
+        Loop
+
+    End Sub
+End Class
